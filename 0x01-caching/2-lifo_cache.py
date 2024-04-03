f41ac695f102df6958e122eac8df9d70259c0a00
Ahmedsaleh1234 <a12661826@gmail.com>
2024-04-03 18:07:40 +0200

    print(f'DISCARD:{discard}')

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,26 +1,15 @@
-#!/usr/bin/python3
-''' LIFO Caching: Create a class LIFOCache that inherits from BaseCaching
-                  and is a caching system
-'''
-
-BaseCaching = __import__('base_caching').BaseCaching
+#!/usr/bin/env python3
+"""2. LIFO Caching"""
+from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    ''' A LIFO Cache.
-        Inherits all behaviors from BaseCaching except, upon any attempt to
-        add an entry to the cache when it is at max capacity (as specified by
-        BaseCaching.MAX_ITEMS), it discards the newest entry to accommodate for
-        the new one.
-        Attributes:
-          __init__ - method that initializes class instance
-          put - method that adds a key/value pair to cache
-          get - method that retrieves a key/value pair from cache '''
+    """LIFOCache that inherits from
+    BaseCaching and is a caching system:"""
 
     def __init__(self):
-        ''' Initialize class instance. '''
         super().__init__()
-        self.keys = []
+        self.keys = list()
 
     def put(self, key, item):
         ''' Add key/value pair to cache data.
@@ -38,8 +27,9 @@ class LIFOCache(BaseCaching):
                 print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        ''' Return value stored in `key` key of cache.
-            If key is None or does not exist in cache, return None. '''
+        """ Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None.
+        """
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
