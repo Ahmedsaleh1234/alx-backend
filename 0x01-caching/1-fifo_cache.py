d79df1b4f2c5ade58a69e95ebf3c836c90b9872a
Ahmedsaleh1234 <a12661826@gmail.com>
2024-04-03 15:18:27 +0200

    DISCARD:

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -21,9 +21,8 @@ class FIFOCache(BaseCaching):
         assign to the dictionary self.cache_data the
         item value for the key key
         """
-        if key is None and item is None:
-            return
-        self.cache_data[key] = item
+        if key is not None and item is not None:
+            self.cache_data[key] = item
         if key not in self.keys:
             self.keys.append(key)
         if (len(self.keys) > BaseCaching.MAX_ITEMS):
