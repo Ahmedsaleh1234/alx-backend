6c7d72ad74857a78c68b8cae53711275db059e74
Ahmedsaleh1234 <a12661826@gmail.com>
2024-04-03 18:02:56 +0200

    print(f'DISCARD:{discard}')

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """2. LIFO Caching"""
 from base_caching import BaseCaching
 
@@ -9,7 +9,7 @@ class LIFOCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
-        self.list_key = ''
+        self.keys = list()
 
     def put(self, key, item):
         """
@@ -18,10 +18,14 @@ class LIFOCache(BaseCaching):
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                print(f'DISCARD:{self.list_key}')
-                self.cache_data.pop(self.list_key)
-            self.list_key = key
+            if key not in self.keys:
+                self.keys.append(key)
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(-2)
+                self.cache_data.pop(discard)
+                print(f'DISCARD:{discard}')
 
     def get(self, key):
         """
