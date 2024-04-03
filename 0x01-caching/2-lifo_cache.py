2277abb66f236b1a5ddae08df1a54f245cf4a801
Ahmedsaleh1234 <a12661826@gmail.com>
2024-04-03 18:06:20 +0200

    print(f'DISCARD:{discard}')

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,21 +1,31 @@
-#!/usr/bin/env python3
-"""2. LIFO Caching"""
-from base_caching import BaseCaching
+#!/usr/bin/python3
+''' LIFO Caching: Create a class LIFOCache that inherits from BaseCaching
+                  and is a caching system
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """LIFOCache that inherits from
-    BaseCaching and is a caching system:"""
+    ''' A LIFO Cache.
+        Inherits all behaviors from BaseCaching except, upon any attempt to
+        add an entry to the cache when it is at max capacity (as specified by
+        BaseCaching.MAX_ITEMS), it discards the newest entry to accommodate for
+        the new one.
+        Attributes:
+          __init__ - method that initializes class instance
+          put - method that adds a key/value pair to cache
+          get - method that retrieves a key/value pair from cache '''
 
     def __init__(self):
+        ''' Initialize class instance. '''
         super().__init__()
-        self.keys = list()
+        self.keys = []
 
     def put(self, key, item):
-        """
-        assign to the dictionary self.cache_data
-        the item value for the key key
-        """
+        ''' Add key/value pair to cache data.
+            If cache is at max capacity (specified by BaseCaching.MAX_ITEMS),
+            discard newest entry in cache to accommodate new entry. '''
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.keys:
@@ -24,13 +34,12 @@ class LIFOCache(BaseCaching):
                 self.keys.append(self.keys.pop(self.keys.index(key)))
             if len(self.keys) > BaseCaching.MAX_ITEMS:
                 discard = self.keys.pop(-2)
-                self.cache_data.pop(discard)
-                print(f'DISCARD:{discard}')
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        """ Return value stored in `key` key of cache.
-            If key is None or does not exist in cache, return None.
-        """
+        ''' Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None. '''
         if key is not None and key in self.cache_data:
             return self.cache_data[key]
         return None
