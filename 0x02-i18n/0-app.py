40361ccc7a961bb143ea6ef236f46eea62541f91
Ahmedsaleh1234 <a12661826@gmail.com>
2024-04-09 23:06:26 +0200

    templates

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
-"""0. Basic Flask app"""
+"""
+A Basic flask application
+"""
+from flask import Flask
+from flask import render_template
 
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """route and an index.html template"""
-    return render_template('index.html')
+def index() -> str:
+    """
+    Renders a basic html template
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
