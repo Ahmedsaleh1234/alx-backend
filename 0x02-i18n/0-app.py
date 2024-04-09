20d441a7820f3a6c0bfdf3a8de9ce8821a15f75c
Ahmedsaleh1234 <a12661826@gmail.com>
2024-04-09 23:49:14 +0200

    0-index.html

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-
-"""Route module for the API
-"""
+"""0. Basic Flask app"""
 
 from flask import Flask, render_template
 
@@ -9,13 +7,10 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def welcome() -> str:
-    """ GET /
-    Return:
-        Hello world
-    """
+def index() -> str:
+    """route and an index.html template"""
     return render_template('index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
